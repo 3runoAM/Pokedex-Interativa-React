d22d62e3e894470fd4f484b7c375ffbdf950881f
Bruno Martins <111077073+3runoAM@users.noreply.github.com>
2025-09-29 17:47:34 -0300

    Finish styling of the register screen (#2)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css';
 import {supabase} from './services/SupabaseClient.js';
-import Login from './pages/Login.jsx';
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
 
 export default function App() {
     console.log('Supabase client:', supabase);
 
     return (
         <>
-            <Login></Login>
+            {/*<Login/>*/}
+
+            <Register/>
         </>
     );
 }
