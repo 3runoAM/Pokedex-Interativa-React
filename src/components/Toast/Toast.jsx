b868161bb95f20f739cdc87f41dbc287085cea71
Bruno Martins <jbrunno17@gmail.com>
2025-11-06 17:25:42 +0000

    Change Toast styling

--- src/components/Toast/Toast.jsx
+++ src/components/Toast/Toast.jsx
@@ -4,9 +4,9 @@ export default function Toast({ message, visible, onClose }) {
     if (!message || !visible) return null;
 
     return (
-        <div className={`${style.toast} mediumPadding flex-column mediumGap`}>
-            <button className={`flex-row flex-center`} onClick={onClose}>×</button>
+        <div className={`${style.toast} flex-row flex-center mediumPadding smallGap`}>
             <p>{message}</p>
+            <button className={`flex-row flex-center`} onClick={onClose}>×</button>
         </div>
     );
 }
